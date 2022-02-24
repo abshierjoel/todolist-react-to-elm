0e478b54a7da0e897c0f4fd7c92cb676d0ee4089
Joel Abshier <jabshier@influxdata.com>
2022-02-23 20:28:06 -0600

    🎉 Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const addItem = (item) => {
+    setItems([item, ...items]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Todos items={items} />
+      <TodoForm addItem={addItem} />
     </div>
   );
 }
+
+const Todos = ({ items }) => (
+  <ul>
+    {items.map((item, i) => (
+      <li key={i}>{item}</li>
+    ))}
+  </ul>
+);
+
+const TodoForm = ({ addItem }) => {
+  const [text, setText] = useState('');
+
+  const handleInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (text.length > 0) {
+      addItem(text);
+      setText('');
+    }
+  };
+
+  return (
+    <>
+      <input type='text' value={text} onChange={handleInput} />
+      <input type='submit' value='Add' onClick={handleSubmit} />
+    </>
+  );
+};
 
 export default App;
